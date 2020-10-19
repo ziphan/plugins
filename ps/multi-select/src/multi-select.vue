b25d5f370b65e3f5f266c5aa2c6092c6e4034d3f
ziphan <ziphan@users.noreply.github.com>
2020-10-19 18:12:40 +0800

    Update multi-select.vue

--- ps/multi-select/src/multi-select.vue
+++ ps/multi-select/src/multi-select.vue
@@ -3,8 +3,6 @@
        使用：
        <ps-multi-select
           v-model="queryCondition.appointstatus.objectvalue"
-          url="/api/scm/app/base/doService"
-          method="post"
           :data="billstatusParams"
           code="code_code"
           name="code_name"
@@ -46,14 +44,14 @@
   -->
   <div class="ps-multi-select">
     <div class="ps-multi-select-view" @click="dialogTableVisible = true">
-      <span v-if="multipleSelection.length < 1" class="placeholder">{{
+      <span v-if="multiSelectTable.length < 1" class="placeholder">{{
         "请选择" || placeholder
       }}</span>
       <span v-else v-html="textView"></span>
       <i
         class="el-icon-close"
         style="right:30px"
-        @click.stop="btnClear"
+        @click.stop="handleClear(2)"
         v-if="value.length > 0"
       ></i>
       <i class="el-icon-arrow-up" :class="{ down: dialogTableVisible }"></i>
@@ -61,6 +59,8 @@
     <el-dialog
       :title="'请选择' + (title ? title : '')"
       :visible.sync="dialogTableVisible"
+      @opened="dialogOpened"
+      @closed="dialogClosed"
     >
       <el-form ref="queryCondition" :model="queryCondition">
         <el-form-item label="代码" prop="code">
@@ -82,14 +82,14 @@
             type="primary"
             icon="el-icon-search"
             size="small"
-            @click="onSubmit"
+            @click="handleSubmit"
             >查询</el-button
           >
           <el-button
             type="default"
             icon="el-icon-refresh"
             size="small"
-            @click="resetForm('queryCondition')"
+            @click="handleReset('queryCondition')"
             >重置</el-button
           >
         </el-form-item>
@@ -98,27 +98,26 @@
       <el-table
         stripe
         border
-        ref="multipleTable"
-        height="240"
+        height="260"
         size="mini"
         v-loading="loading"
+        ref="multiSelectTable"
         :data="tableData"
         :row-key="getRowKey"
+        @row-dblclick="handleToggleSelect"
         @selection-change="handleSelectionChange"
-        @select="handleSelect"
-        @row-dblclick="toggleSelect"
       >
-        <el-table-column
-          align="center"
-          type="selection"
-          width="45"
-          :reserve-selection="true"
-        ></el-table-column>
         <el-table-column
           align="center"
           :prop="id_name"
           width="60"
           label="序号"
+        ></el-table-column>
+        <el-table-column
+          align="center"
+          type="selection"
+          width="45"
+          :reserve-selection="true"
         ></el-table-column>
         <el-table-column
           align="center"
@@ -143,19 +142,26 @@
       </div>
       <el-divider></el-divider>
       <div class="selected-container">
+        <div class="selected-none" v-if="multiSelectTable.length == 0">
+          请从列表中选择数据
+        </div>
         <el-tag
-          v-for="item in multipleSelection"
+          v-else
+          v-for="item in multiSelectTable"
           :key="item[id_name]"
           closable
-          @close="toggleSelect(item)"
-          >{{ item[name] }}</el-tag
+          @close="handleToggleSelect(item)"
         >
+          {{ item[name] }}
+        </el-tag>
       </div>
 
       <div style="text-align:right">
-        <el-button @click="btnClear">清空</el-button>
-        <el-button type="primary" @click="btnOK">确定</el-button>
-        <!-- <el-button @click="btnCancel">取消</el-button> -->
+        <el-button size="small" @click="handleClear">清空</el-button>
+        <el-button size="small" @click="handleCancel">取消</el-button>
+        <el-button size="small" type="primary" @click="handleOK"
+          >确定</el-button
+        >
       </div>
     </el-dialog>
   </div>
@@ -164,39 +170,33 @@
 <script>
 import { getDoService } from "@/request/api";
 import { htmlEncode } from "@/utils/index";
+
 export default {
   name: "PsMultiSelect",
   data() {
     return {
-      queryConditionLabelWidth: "100px",
+      textView: "",
+      codeLink: "",
       dialogTableVisible: false,
-      tableData: [],
+      multiSelectTable: [],
+      cachedMultiSelectTable: [],
       queryCondition: {
         code: "",
         name: ""
       },
+      params: JSON.parse(JSON.stringify(this.data)),
+      tableData: [],
       loading: true,
-      multipleSelection: [],
-      textView: "",
       currentPage: 1,
-      total: 0,
-      params: this.data,
-      loadedPages: [],
-      loadedPageContent: {}
+      total: 0
     };
-  },
-  model: {
-    prop: "value",
-    event: "click"
   },
   props: {
     value: String,
     placeholder: String,
-    url: String,
-    method: String,
     data: Object,
-    name: String,
     code: String,
+    name: String,
     title: String,
     idn: String
   },
@@ -215,103 +215,62 @@ export default {
       }
     }
   },
-  watch: {
-    dialogTableVisible(nv) {
-      if (nv) {
-        this.getTableData(1);
-      } else {
-        this.loading = true;
-      }
-    },
-    // 外部清空选项时，将组建内部数据清空
-    value(nv) {
-      if (nv == "" && this.$refs.multipleTable) {
-        this.$refs.multipleTable.clearSelection();
-        this.multipleSelection = [];
-      }
-    }
+  /**
+   * 自定义组件 v-model
+   */
+  model: {
+    prop: "value",
+    event: "click"
   },
   methods: {
-    onSubmit() {
-      this.$refs.multipleTable.clearSelection();
-      this.loadedPages = [];
-      this.loadedPageContent = {};
-      this.currentPage = 1;
-      this.getTableData(1);
-    },
-    resetForm(formName) {
-      this.$refs[formName].resetFields();
-    },
-    handleSelectionChange(val) {
-      this.multipleSelection = val;
-    },
-    handleSelect(val, row) {
-      return { val, row };
-    },
-    toggleSelect(row) {
-      this.$refs.multipleTable.toggleRowSelection(row);
-    },
-    handleCurrentChange(val) {
-      this.loading = true;
-      this.handlePageChange(val);
-    },
-    handlePageChange(val) {
-      const params = this.params;
-      params.bizContent.jsonvalue.pager.pageno = val;
-      this.params = params;
-      this.getTableData(val);
+    dialogOpened() {
+      this.getTableData(this.params);
     },
     /**
-     * 获取列表数据
-     * @param {number} n 当前页数
-     * 如果 loadedPages 中已有当前页数据，则直接从 loadedPages 中提取数据
-     * 如果 loadedPages 中没有当前页数据，则按照查询条件，从后台获取新的数据，并将其存入到 loadedPages 中，以供下次使用
-     *
+     * 窗口关闭时，如果数据有变化，则执行取消动作
+     */
+    dialogClosed() {
+      if (this.multiSelectTable != this.cachedMultiSelectTable) {
+        this.handleCancel();
+      }
+    },
+    /**
      * 出于安全考虑，表单形式的请求参数需要 encode 之后再传入到后台
      */
-    getTableData(n) {
-      new Promise(resolve => {
-        if (this.loadedPages.indexOf(n) < 0) {
-          const params = JSON.parse(JSON.stringify(this.data));
-          params.bizContent.jsonvalue.pager.pageno = n;
-          params.bizContent.jsonvalue.data[0].objectvalue = htmlEncode(
-            this.queryCondition.code
-          );
-          params.bizContent.jsonvalue.data[1].objectvalue = htmlEncode(
-            this.queryCondition.name
-          );
-          params.bizContent.jsonvalue.data = params.bizContent.jsonvalue.data.filter(
-            item => item.objectvalue != ""
-          );
-          getDoService(params)
-            .then(res => {
-              const data = JSON.parse(res.data.returnJson);
-              this.tableData = data.arrys;
-              this.total = data.total;
-              this.loadedPages.push(n);
-              this.loadedPageContent["page" + n] = this.tableData;
-            })
-            .then(() => {
-              resolve();
-            });
-        } else {
-          this.tableData = this.loadedPageContent["page" + n];
-          this.currentPage = n;
-          resolve();
-        }
-      }).then(() => {
-        this.loading = false;
-      });
+    handleSubmit() {
+      this.loading = true;
+      this.$refs.multiSelectTable.clearSelection();
+      this.setJsonvalue(
+        htmlEncode(this.queryCondition.code),
+        htmlEncode(this.queryCondition.name)
+      );
+      this.params.bizContent.jsonvalue.pager.pageno = 1;
+      const params = JSON.parse(JSON.stringify(this.params));
+      params.bizContent.jsonvalue.data = params.bizContent.jsonvalue.data.filter(
+        item => item.objectvalue != ""
+      );
+      this.getTableData(params);
     },
-    getRowKey(row) {
-      return row[this.id_name];
+    /**
+     * 重置查询条件，会重新拉取列表
+     */
+    handleReset(formName) {
+      this.loading = true;
+      this.$refs[formName].resetFields();
+      this.setJsonvalue();
+      this.params.bizContent.jsonvalue.pager.pageno = 1;
+      this.getTableData(this.params);
     },
-    btnOK() {
-      const texts = this.multipleSelection.reduce(
+    /**
+     * 确定动作，将 codeLink 传递到外部
+     */
+    handleOK() {
+      this.cachedMultiSelectTable = this.multiSelectTable;
+      const texts = this.multiSelectTable.reduce(
         (result = [], item) => result.concat(item[this.name]),
         []
       );
-      const codes = this.multipleSelection.reduce(
+      const codes = this.multiSelectTable.reduce(
         (result = [], item) => result.concat(item[this.code]),
         []
       );
@@ -320,31 +279,100 @@ export default {
       this.$emit("click", this.codeLink);
       this.dialogTableVisible = false;
     },
-    // btnCancel() {
-    //   this.multipleSelection = this.old_multipleSelection;
-    //   this.loadedPageContent = this.old_loadedPageContent;
-    //   this.dialogTableVisible = false;
-    // }
-    btnClear() {
-      this.$refs.multipleTable.clearSelection();
-      this.multipleSelection = [];
-      this.$emit("click", "");
+    /**
+     * 清除动作
+     * type = 1 : 临时清除，可取消
+     * type = 2 : 清除并保存
+     */
+    handleClear(type = 1) {
+      this.$refs.multiSelectTable.clearSelection();
+      this.multiSelectTable = [];
+      if (type == 2) {
+        this.cachedMultiSelectTable = [];
+        this.$emit("click", "");
+      }
+    },
+    /**
+     * 取消动作，将不保留此次的操作
+     */
+    handleCancel() {
+      const diff_1 = this.multiSelectTable.filter(item => {
+        return (
+          Array.prototype.indexOf.call(this.cachedMultiSelectTable, item) < 0
+        );
+      });
+      const diff_2 = this.cachedMultiSelectTable.filter(item => {
+        return Array.prototype.indexOf.call(this.multiSelectTable, item) < 0;
+      });
+      const diff = [...diff_1, ...diff_2];
+      diff.forEach(item => {
+        this.handleToggleSelect(item);
+      });
+      this.multiSelectTable = this.cachedMultiSelectTable;
+      this.dialogTableVisible = false;
+    },
+    handleSelectionChange(val) {
+      this.multiSelectTable = val;
+    },
+    handleToggleSelect(row) {
+      this.$refs.multiSelectTable.toggleRowSelection(row);
+    },
+    handleCurrentChange(val) {
+      this.loading = true;
+      this.params.bizContent.jsonvalue.pager.pageno = val;
+      this.getTableData(this.params);
+    },
+    getTableData(params) {
+      getDoService(params)
+        .then(res => {
+          const returnJson = JSON.parse(res.data.returnJson);
+          this.tableData = returnJson.arrys;
+          this.total = returnJson.total;
+        })
+        .then(() => {
+          this.loading = false;
+        });
+    },
+    /**
+     * 缓存多页选项，将多个页面选中的 item 同时保存在 multiSelectTable
+     */
+    getRowKey(row) {
+      return row[this.id_name];
+    },
+    /**
+     * 设置查询条件 name 和 code 的值
+     * value1: code
+     * value2: name
+     */
+    setJsonvalue(value1 = "", value2 = "") {
+      this.params.bizContent.jsonvalue.data[0].objectvalue = value1;
+      this.params.bizContent.jsonvalue.data[1].objectvalue = value2;
     }
   }
 };
 </script>
-
 
 <style lang="scss">
 @import "../../mixins/queryCondition.scss";
 .ps-multi-select {
   @include mini-dialog;
   @include wrapper;
+  th {
+    line-height: 24px !important;
+  }
   .ps-multi-select-view {
     @include view;
   }
   .selected-container {
-    min-height: 36px;
+    margin: 8px 0;
+    height: 80px;
+    overflow: auto;
+    background: #fefefe;
+    border: 1px solid #ebeef5;
+    .selected-none {
+      padding: 0 10px;
+      color: #c1c1c1;
+    }
   }
   .el-tag {
     margin: 0 4px;
@@ -355,6 +383,14 @@ export default {
   }
   .el-form-item {
     width: 30% !important;
+  }
+  .el-dialog {
+    height: 560px;
+  }
+  .el-dialog__body {
+    padding-bottom: 0;
+    max-height: initial;
+    height: 515px;
   }
 }
 </style>
